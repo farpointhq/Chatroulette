588952319e3f0da8bc49191a82bc5056059c6cba
Ryan Monsurate <ryanmonsurate@users.noreply.github.com>
2026-06-01 18:22:43 -0700

    test(issue-8): fix vitest config for React component tests
    Change environment from 'node' to 'jsdom' to enable DOM testing with @testing-library/react.
    Tests currently fail as expected - components are stubs that need implementation.

---
+++ src/components/Lobby.ts
@@ -0,0 +1,3 @@
+// Barrel export for Lobby component - re-exports from lobby folder
+export { Lobby } from './lobby/Lobby';
+export type { LobbyProps, LobbyStatus, PlayerInfo, MatchFoundData } from './lobby/types';
